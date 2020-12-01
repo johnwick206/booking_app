ffc08d8b710c6ceeeb9362c10bc4d3a39f21a32f
unknown <pratikborhade302@gmail.com>
2020-12-02 01:21:09 +0530

    user static app done

---
+++ booking_app/app/src/main/java/com/example/booking_app/DialogBoxRB.java
@@ -0,0 +1,50 @@
+package com.example.booking_app;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.DialogFragment;
+
+public class DialogBoxRB extends DialogFragment {
+    private static onClickBlocks blockInterfaceRef;
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(@Nullable Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        final String[] list = getActivity().getResources().getStringArray(R.array.Blocks);
+        builder.setTitle("Block")
+                .setSingleChoiceItems(list, 0, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        blockInterfaceRef.setBlockName(list[i]);
+                    }
+                })
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //keep blank
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        //keep blank
+                    }
+                });
+        return builder.create();
+    }
+
+    public interface onClickBlocks{
+        void setBlockName(String blockName);
+    }
+
+    public static void setOnCLickBlockOptn(onClickBlocks onClickBlocks){
+        blockInterfaceRef = onClickBlocks;
+    }
+}
---
+++ booking_app/app/src/main/java/com/example/booking_app/FormActivity.java
@@ -0,0 +1,28 @@
+package com.example.booking_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class FormActivity extends AppCompatActivity {
+
+    private TextView roomTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_form);
+
+        View decorView = getWindow().getDecorView();
+        // Hide the status bar.
+        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+        decorView.setSystemUiVisibility(uiOptions);
+
+        roomTitle = findViewById(R.id.RoomName);
+        String name = getIntent().getStringExtra("room");
+        roomTitle.setText(name);
+
+    }
+}
