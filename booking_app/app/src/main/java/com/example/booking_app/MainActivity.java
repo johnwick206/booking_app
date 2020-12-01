61be6feee11cf0d7bb911a3ea013af134f97552b
unknown <pratikborhade302@gmail.com>
2020-12-01 19:08:30 +0530

    sign ip , sign up , category Ui added

--- booking_app/app/src/main/java/com/example/booking_app/MainActivity.java
+++ booking_app/app/src/main/java/com/example/booking_app/MainActivity.java
@@ -4,16 +4,66 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+	private Button googleLoginBtn , twitterLoginBtn , fbLoginBtn , loginBtn;
+	private TextView moveToSignUp;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		// TODO: 01-12-2020 switch to bookiing category
-		/*startActivity(new Intent(MainActivity.this , CategoryActivity.class));
-		finish();*/
+		referenceUi();
+		setClickListener();
+	}
+
+	private void referenceUi() {
+		loginBtn = findViewById(R.id.LoginBtn);
+		googleLoginBtn = findViewById(R.id.GoogleLoginBtn);
+		twitterLoginBtn = findViewById(R.id.TwitterLoginBtn);
+		fbLoginBtn = findViewById(R.id.FBLoginBtn);
+		moveToSignUp = findViewById(R.id.MoveToSIgnUp);
+	}
+
+	private void setClickListener() {
+
+		loginBtn.setOnClickListener(this);
+		googleLoginBtn.setOnClickListener(this);
+		twitterLoginBtn.setOnClickListener(this);
+		fbLoginBtn.setOnClickListener(this);
+		moveToSignUp.setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View view) {
+		switch(view.getId()){
+			case R.id.LoginBtn : login(); break;
+			case R.id.GoogleLoginBtn : googleLogin();  break;
+			case R.id.TwitterLoginBtn : twitterLogin(); break;
+			case R.id.FBLoginBtn : fbLogin(); break;
+			case R.id.MoveToSIgnUp : moveTosignUpPage(); break;
+		}
+	}
+
+	private void login() {
+		startActivity(new Intent(MainActivity.this , CategoryActivity.class));
+	}
+
+	private void googleLogin() {
+	}
+
+	private void twitterLogin() {
+	}
+
+	private void fbLogin() {
+	}
+
+	private void moveTosignUpPage() {
+		startActivity(new Intent(MainActivity.this , SignUp.class));
 	}
 }
