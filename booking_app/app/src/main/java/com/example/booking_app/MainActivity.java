a5713ad4839b08b8134ef51646d624af75fafdc4
unknown <pratikborhade302@gmail.com>
2020-12-03 01:24:47 +0530

    sign in with google , Mail ID --done--

--- booking_app/app/src/main/java/com/example/booking_app/MainActivity.java
+++ booking_app/app/src/main/java/com/example/booking_app/MainActivity.java
@@ -1,27 +1,67 @@
 package com.example.booking_app;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
 	private Button googleLoginBtn , twitterLoginBtn , fbLoginBtn , loginBtn;
 	private TextView moveToSignUp;
+	private EditText emailT , passwordT;
+
+	private FirebaseAuth mAuth;
+	private ProgressDialog progressDialog;
+
+	GoogleSignInClient mGoogleSignInClient;
+	private String TAG = "MainActivity";
+	private int RC_SIGN_IN = 1;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		mAuth = FirebaseAuth.getInstance();
+
+		GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).requestIdToken(getString(R.string.default_web_client_id))
+				.requestEmail()
+				.build();
+
+		mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
 		referenceUi();
 		setClickListener();
 	}
 
 	private void referenceUi() {
+
+		emailT = findViewById(R.id.emailED);
+		passwordT = findViewById(R.id.PassWordED);
+
 		loginBtn = findViewById(R.id.LoginBtn);
 		googleLoginBtn = findViewById(R.id.GoogleLoginBtn);
 		twitterLoginBtn = findViewById(R.id.TwitterLoginBtn);
@@ -51,11 +91,60 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	}
 
 	private void login() {
-		startActivity(new Intent(MainActivity.this , CategoryActivity.class));
+
+		final String emailS , passwordS;
+		emailS = emailT.getText().toString().trim();
+		passwordS = passwordT.getText().toString().trim();
+
+		if(TextUtils.isEmpty(emailS)){
+			emailT.setError("Enter valid Email-id");
+			return;
+		}
+		if(TextUtils.isEmpty(passwordS)){
+			passwordT.setError("Enter password");
+			return;
+		}
+
+
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setTitle("Login");
+		progressDialog.setMessage("Getting in....");
+		progressDialog.setCanceledOnTouchOutside(false);
+		progressDialog.show();
+
+		mAuth.signInWithEmailAndPassword(emailS , passwordS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(@NonNull Task<AuthResult> task) {
+
+				if(task.isSuccessful()){
+					progressDialog.dismiss();
+
+					// TODO: 03-12-2020 Check admin or not
+					if(isAdmin(emailS , passwordS)) {
+						startActivity(new Intent(MainActivity.this , AdminMain.class));
+						finish();
+						return;
+					}
+
+					startActivity(new Intent(MainActivity.this , CategoryActivity.class));
+					Toast.makeText(MainActivity.this, "Successful", Toast.LENGTH_SHORT).show();
+					finish();
+				}else {
+					progressDialog.dismiss();
+					Toast.makeText(MainActivity.this, "Check Credentials" + task.getException(), Toast.LENGTH_SHORT).show();
+				}
+			}
+		});
+
+	}
+
+	private boolean isAdmin(String email , String password){
+		return email.equals("adminvit2125@gmail.com") && password.equals("123456");
 	}
 
 	private void googleLogin() {
-		startActivity(new Intent(MainActivity.this , AdminMain.class));
+		Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+		startActivityForResult(signInIntent,RC_SIGN_IN);
 	}
 
 	private void twitterLogin() {
@@ -66,5 +155,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 	private void moveTosignUpPage() {
 		startActivity(new Intent(MainActivity.this , SignUp.class));
+	}
+
+
+	protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		if(requestCode == RC_SIGN_IN){
+			Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+			handlesSignInResult(task);
+		}
+	}
+	private void  handlesSignInResult (Task<GoogleSignInAccount> completedTask){
+		try {
+			GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
+			Toast.makeText(MainActivity.this, "Signed in Successfully", Toast.LENGTH_SHORT).show();
+			FirebaseGoogleAuth(acc);
+		} catch (ApiException e) {
+			Toast.makeText(MainActivity.this, "Signed in Failed",Toast.LENGTH_SHORT).show();
+			FirebaseGoogleAuth(null);
+
+		}
+	}
+
+	private void FirebaseGoogleAuth(GoogleSignInAccount acct){
+		AuthCredential authCredential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+		mAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+			@Override
+			public void onComplete(@NonNull Task<AuthResult> task) {
+				if(task.isSuccessful()){
+					Toast.makeText(MainActivity.this, "Success",Toast.LENGTH_SHORT).show();
+					FirebaseUser user = mAuth.getCurrentUser();
+					updateUI(user);
+
+				}
+				else{
+					Toast.makeText(MainActivity.this, "Failed",Toast.LENGTH_SHORT).show();
+					updateUI(null);
+				}
+
+			}
+		});
+
+
+
+	}
+	private void updateUI(FirebaseUser fUser){
+		//next page code
+		GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+		if (account != null){
+			String personName = account.getDisplayName();
+			String personGivenName = account.getGivenName();
+			String personFamilyName = account.getFamilyName();
+			String personEmail = account.getEmail();
+			String personId = account.getId();
+
+			Toast.makeText(MainActivity.this, personName + personEmail, Toast.LENGTH_SHORT).show();
+		}
+
+		referenceUi();
+		setClickListener();
+		startActivity(new Intent(MainActivity.this , CategoryActivity.class));
+		finish();
 	}
 }
--- booking_app/app/src/main/java/com/example/booking_app/SignUp.java
+++ booking_app/app/src/main/java/com/example/booking_app/SignUp.java
@@ -1,14 +1,101 @@
 package com.example.booking_app;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SignUp extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class SignUp extends AppCompatActivity implements View.OnClickListener {
+
+   private EditText nameT , emailT , passwordT;
+   private Button signUpBtn ,googleBtn , twitterBtn , fbBtn;
+   private FirebaseAuth mAuth;
+   private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        refUI();
+        signUpBtn.setOnClickListener(this);
+    }
+
+    private void refUI() {
+
+        nameT = findViewById(R.id.UserNameED);
+        emailT = findViewById(R.id.EmailED);
+        passwordT = findViewById(R.id.PasswordED);
+        signUpBtn = findViewById(R.id.SignUpBtn);
+        /*googleBtn = findViewById(R.id.GoogleLoginBtn);
+        twitterBtn = findViewById(R.id.TwitterLoginBtn);
+        fbBtn = findViewById(R.id.FBLoginBtn);*/
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.SignUpBtn) {
+            signUpWithEmail();
+        }
+    }
+
+    private void signUpWithEmail() {
+
+        String nameS , emailS , passwordS;
+        nameS = nameT.getText().toString();
+        emailS = emailT.getText().toString().trim();
+        passwordS = passwordT.getText().toString().trim();
+
+        if(TextUtils.isEmpty(nameS)){
+            nameT.setError("Enter name");
+            return;
+        }
+        if(TextUtils.isEmpty(emailS)){
+            emailT.setError("Enter valid email-id");
+            return;
+        }
+        if(TextUtils.isEmpty(passwordS)){
+            passwordT.setError("Enter password");
+            return;
+        }
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Login");
+        progressDialog.setMessage("Initializing....");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        //create new Account
+        mAuth.createUserWithEmailAndPassword(emailS , passwordS).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()) {
+                    FirebaseUser user = mAuth.getCurrentUser();
+                    startActivity(new Intent(SignUp.this, CategoryActivity.class));
+                    Toast.makeText(SignUp.this, "Sign Up complete", Toast.LENGTH_SHORT).show();
+                    finish();
+                }else {
+                    progressDialog.dismiss();
+                    Toast.makeText(SignUp.this, "Sign Up failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 }
