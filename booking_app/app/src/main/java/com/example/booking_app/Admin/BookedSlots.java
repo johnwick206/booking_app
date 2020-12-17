b45d22d7305c4dd9b644d36407c0eed8e6498417
Heramba Limaye <prasannalimaye19@gmail.com>
2020-12-17 16:06:43 +0530

    half assed dlt button

--- booking_app/app/src/main/java/com/example/booking_app/Admin/BookedSlots.java
+++ booking_app/app/src/main/java/com/example/booking_app/Admin/BookedSlots.java
@@ -16,13 +16,17 @@ import com.example.booking_app.RoomDetails;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 public class BookedSlots extends AppCompatActivity {
@@ -31,6 +35,7 @@ public class BookedSlots extends AppCompatActivity {
     AdminEventAdapter adapter;
     RecyclerView recyclerView;
     ArrayList<String> list = new ArrayList<>();
+    CollectionReference datedelref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +44,8 @@ public class BookedSlots extends AppCompatActivity {
 
         recyclerView = findViewById(R.id.recylcerView);
         fireStore = FirebaseFirestore.getInstance();
-
+          datedelref=fireStore.collection("Date").document("30-30-3030").collection(RoomDetails.roomType)
+                .document("block").collection(RoomDetails.roomBlock);
 
         String collectionName = RoomDetails.roomType;
 
@@ -55,7 +61,43 @@ public class BookedSlots extends AppCompatActivity {
                 intent.putExtra("event Name" , name);
                 startActivity(intent);
             }
+
+            @Override
+            public void deleteEvents(final String slot, final String date, String name, final String roomNo) {
+                CollectionReference delref=fireStore.collection( "Booking: "+RoomDetails.roomType);
+                delref.document(name).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        dateDelete(date,slot,roomNo);
+                    }
+                });
+            }
         });
+    }
+
+    private void dateDelete(String date, final String slot, final String roomNo) {
+         datedelref=fireStore.collection("Date").document(date).collection(RoomDetails.roomType)
+                .document("block").collection(RoomDetails.roomBlock);
+
+        datedelref.document(roomNo).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                String dateSlotOld=task.getResult().get("slots").toString();
+                int updatedSlot=Integer.parseInt(dateSlotOld)-Integer.parseInt(slot);
+                HashMap<String,Object> map=new HashMap<>();
+                map.put("slots",Integer.toString(updatedSlot));
+                datedelref.document(roomNo).update(map).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(BookedSlots.this, "Seminar Deleted", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+
+
     }
 
     private void adapterFn(String collectionName){
